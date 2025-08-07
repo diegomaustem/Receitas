b6572792a6e52f059ab93b73fb6fd957bc006e25
diegomaustem <diego.maustem@gmail.com>
2025-08-07 10:15:40 -0300

    :sparkles: feat: Criação do servidor backend

--- backend/src/http/servidor.ts
+++ backend/src/http/servidor.ts
@@ -1,1 +1,27 @@
-console.log("Running servidor.ts");
+import express, { Express } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+class Servidor {
+  private servidor: Express;
+  private porta: number | string;
+
+  constructor() {
+    this.servidor = express();
+    this.porta = process.env.PORTA || 3003;
+    this.middlewares();
+  }
+
+  private middlewares(): void {
+    this.servidor.use(express.json());
+  }
+
+  public iniciaServidor(): void {
+    this.servidor.listen(this.porta, () => {
+      console.log("Servidor on na porta:", this.porta);
+    });
+  }
+}
+
+const servidor = new Servidor();
+servidor.iniciaServidor();
