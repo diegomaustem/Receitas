e252babef35413230a984cee8babb11c65507345
diegomaustem <diego.maustem@gmail.com>
2025-08-08 11:47:32 -0300

    :recycle: refactor: Acrescenta rotas de receitas

--- backend/src/routes/rotas.ts
+++ backend/src/routes/rotas.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import { usuarioController } from "../controllers/UsuarioController";
+import { receitaController } from "../controllers/ReceitaController";
 import { validate } from "../middlewares/middlewareValidacao";
 import {
   schemaAtualizacaoUsuario,
   schemaCriacaoUsuario,
 } from "../validations/ValidacaoUsuario";
+import {
+  schemaAtualizacaoReceita,
+  schemaCriacaoReceita,
+} from "../validations/ValidacaoReceita";
 const rotas = Router();
 
+// Usuários
 rotas.get("/usuarios", usuarioController.listarUsuarios);
 rotas.get("/usuario/:id", usuarioController.listarUsuario);
 rotas.post(
@@ -20,5 +26,20 @@ rotas.patch(
   usuarioController.atualizarUsuario
 );
 rotas.delete("/usuario/:id", usuarioController.excluirUsuario);
+
+// Receitas
+rotas.get("/receitas", receitaController.listarReceitas);
+rotas.get("/receita/:id", receitaController.listarReceita);
+rotas.post(
+  "/receita",
+  validate(schemaCriacaoReceita, "body"),
+  receitaController.criarReceita
+);
+rotas.patch(
+  "/receita/:id",
+  validate(schemaAtualizacaoReceita, "body"),
+  receitaController.atualizarReceita
+);
+rotas.delete("/receita/:id", receitaController.excluirReceita);
 
 export default rotas;
