f1470b9694b107d77e95df96b0671c0d9fec9961
diegomaustem <diego.maustem@gmail.com>
2025-08-07 22:26:23 -0300

    :sparkles: feat: Criação do service de usuários

---
+++ backend/src/services/ServicoUsuario.ts
@@ -0,0 +1,112 @@
+import { IUsuario } from "../Interfaces/IUsuario";
+import { utilsSenha } from "../utils/UtilsSenha";
+import { repositorioGenerico } from "../repositories/RepositorioGenerico";
+import RepositorioUsuario from "../repositories/RepositorioUsuario";
+import HttpErro from "../errors/HttpErros";
+
+class ServicoUsuario {
+  async listarUsuarios(): Promise<IUsuario[]> {
+    try {
+      return await RepositorioUsuario.listarUsuarios();
+    } catch (error) {
+      console.error(":S - Falha ao recuperar usuários.", error);
+      throw error;
+    }
+  }
+
+  async listarUsuario(idUsuario: number): Promise<IUsuario | null> {
+    try {
+      return await RepositorioUsuario.listarUsuario(idUsuario);
+    } catch (error) {
+      console.error(":S - Falha ao recuperar usuário.", error);
+      throw error;
+    }
+  }
+
+  async criarUsuario(usuario: IUsuario): Promise<IUsuario> {
+    try {
+      const senhaComHash = await utilsSenha.hashSenha(usuario.senha);
+
+      const novoUsuario: IUsuario = {
+        ...usuario,
+        senha: senhaComHash,
+      };
+
+      return await RepositorioUsuario.criarUsuario(novoUsuario);
+    } catch (error) {
+      console.error(":S - Falha ao criar usuário.", error);
+      throw error;
+    }
+  }
+
+  async atualizarUsuario(
+    idUsuario: number,
+    dadosUsuario: IUsuario
+  ): Promise<IUsuario> {
+    try {
+      if (dadosUsuario.senha) {
+        dadosUsuario.senha = await utilsSenha.hashSenha(dadosUsuario.senha);
+      }
+
+      return await RepositorioUsuario.atualizarUsuario(idUsuario, dadosUsuario);
+    } catch (error) {
+      console.error(":S - Falha ao atualizar usuário.", error);
+      throw error;
+    }
+  }
+
+  async excluirUsuario(idUsuario: number): Promise<IUsuario> {
+    try {
+      return await RepositorioUsuario.excluirUsuario(idUsuario);
+    } catch (error) {
+      console.error(":S - Falha ao excluir usuário.", error);
+      throw error;
+    }
+  }
+
+  async validarRegrasUsuario(
+    dadosUsuario?: IUsuario,
+    idUsuario?: number
+  ): Promise<void> {
+    if (idUsuario) {
+      try {
+        const usuarioTemReceita = await repositorioGenerico.queryGenerica(
+          "receita",
+          "idUsuarios",
+          idUsuario
+        );
+        if (usuarioTemReceita) {
+          throw new HttpErro(
+            "O usuário possuí receitas e não pode ser excluído.",
+            409
+          );
+        }
+        return;
+      } catch (error) {
+        console.error(":S - Falha ao validar regra receita do usuário.", error);
+        throw error;
+      }
+    }
+    if (dadosUsuario?.login) {
+      try {
+        const { login } = dadosUsuario;
+        const loginEmUso = await repositorioGenerico.queryGenerica(
+          "usuario",
+          "login",
+          login
+        );
+
+        if (loginEmUso) {
+          throw new HttpErro(
+            "O login informado já está em uso. Tente outro.",
+            409
+          );
+        }
+      } catch (error) {
+        console.error(":S - Falha ao validar regra login do usuário.", error);
+        throw error;
+      }
+    }
+  }
+}
+export const servicoUsuario = new ServicoUsuario();
