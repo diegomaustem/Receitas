88f9e39245bcb7fb1e537517cd58c3d819d4ddad
diegomaustem <diego.maustem@gmail.com>
2025-08-08 09:08:07 -0300

    :sparkles: feat: Acrescentando rotas junto com middleware de validação de criação e atualização

--- backend/src/routes/rotas.ts
+++ backend/src/routes/rotas.ts
@@ -1,11 +1,24 @@
 import { Router } from "express";
 import { usuarioController } from "../controllers/UsuarioController";
+import { validate } from "../middlewares/middlewareValidacao";
+import {
+  schemaAtualizacaoUsuario,
+  schemaCriacaoUsuario,
+} from "../validations/ValidacaoUsuario";
 const rotas = Router();
 
 rotas.get("/usuarios", usuarioController.listarUsuarios);
 rotas.get("/usuario/:id", usuarioController.listarUsuario);
-rotas.post("/usuario", usuarioController.criarUsuario);
-rotas.patch("/usuario/:id", usuarioController.atualizarUsuario);
+rotas.post(
+  "/usuario",
+  validate(schemaCriacaoUsuario, "body"),
+  usuarioController.criarUsuario
+);
+rotas.patch(
+  "/usuario/:id",
+  validate(schemaAtualizacaoUsuario, "body"),
+  usuarioController.atualizarUsuario
+);
 rotas.delete("/usuario/:id", usuarioController.excluirUsuario);
 
 export default rotas;
