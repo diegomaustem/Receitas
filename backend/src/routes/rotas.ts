03825eced4669b35df23dbaef8b0deeaa0e9e7df
diegomaustem <diego.maustem@gmail.com>
2025-08-08 16:04:56 -0300

    :sparkles: feat: Criação de middleware em rotas

--- backend/src/routes/rotas.ts
+++ backend/src/routes/rotas.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { usuarioController } from "../controllers/UsuarioController";
 import { receitaController } from "../controllers/ReceitaController";
 import { validate } from "../middlewares/middlewareValidacao";
+import { schemaLogin } from "../validations/ValidacaoLogin";
 import {
   schemaAtualizacaoUsuario,
   schemaCriacaoUsuario,
@@ -11,40 +12,54 @@ import {
   schemaCriacaoReceita,
 } from "../validations/ValidacaoReceita";
 import { categoriaController } from "../controllers/CategoriaController";
+import { autenticacaoController } from "../controllers/AutenticacaoController";
+import { auth } from "../middlewares/middlewareAutenticacao";
+
 const rotas = Router();
 
-// Usuários
-rotas.get("/usuarios", usuarioController.listarUsuarios);
-rotas.get("/usuario/:id", usuarioController.listarUsuario);
+// Autenticação :::
+rotas.post(
+  "/login",
+  validate(schemaLogin, "body"),
+  autenticacaoController.login
+);
+
+// Usuários :::
+rotas.get("/usuarios", auth, usuarioController.listarUsuarios);
+rotas.get("/usuario/:id", auth, usuarioController.listarUsuario);
 rotas.post(
   "/usuario",
+  auth,
   validate(schemaCriacaoUsuario, "body"),
   usuarioController.criarUsuario
 );
 rotas.patch(
   "/usuario/:id",
+  auth,
   validate(schemaAtualizacaoUsuario, "body"),
   usuarioController.atualizarUsuario
 );
-rotas.delete("/usuario/:id", usuarioController.excluirUsuario);
+rotas.delete("/usuario/:id", auth, usuarioController.excluirUsuario);
 
-// Receitas
-rotas.get("/receitas", receitaController.listarReceitas);
-rotas.get("/receita/:id", receitaController.listarReceita);
+// Receitas :::
+rotas.get("/receitas", auth, receitaController.listarReceitas);
+rotas.get("/receita/:id", auth, receitaController.listarReceita);
 rotas.post(
   "/receita",
+  auth,
   validate(schemaCriacaoReceita, "body"),
   receitaController.criarReceita
 );
 rotas.patch(
   "/receita/:id",
+  auth,
   validate(schemaAtualizacaoReceita, "body"),
   receitaController.atualizarReceita
 );
-rotas.delete("/receita/:id", receitaController.excluirReceita);
+rotas.delete("/receita/:id", auth, receitaController.excluirReceita);
 
-// Categorias
-rotas.get("/categorias", categoriaController.listarCategorias);
-rotas.get("/categoria/:id", categoriaController.listarCategoria);
+// Categorias :::
+rotas.get("/categorias", auth, categoriaController.listarCategorias);
+rotas.get("/categoria/:id", auth, categoriaController.listarCategoria);
 
 export default rotas;
