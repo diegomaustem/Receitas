1434d342ff4f47fc8930d546e97f2bc2fbf2e801
diegomaustem <diego.maustem@gmail.com>
2025-08-08 13:51:11 -0300

    :recycle: refactor: Criação paginação em repositorio de categoria

--- backend/src/repositories/RepositorioCategoria.ts
+++ backend/src/repositories/RepositorioCategoria.ts
@@ -1,10 +1,35 @@
 import { ICategoria } from "../Interfaces/ICategoria";
+import { IPaginacao } from "../Interfaces/IPaginacao";
+import { IResultadoPaginado } from "../Interfaces/IResultadoPaginado";
 import prisma from "../lib/prismaClient";
 
 class RepositorioCategoria {
-  async listarCategorias(): Promise<ICategoria[]> {
+  async listarCategorias(
+    paginacao: IPaginacao
+  ): Promise<IResultadoPaginado<ICategoria>> {
+    const { pagina, limite } = paginacao;
     try {
-      return await prisma.categoria.findMany();
+      const skip = (pagina - 1) * limite;
+
+      const [dados, total] = await Promise.all([
+        prisma.categoria.findMany({
+          skip,
+          take: limite,
+        }),
+        prisma.categoria.count(),
+      ]);
+
+      const totalPaginas = Math.ceil(total / limite);
+
+      return {
+        dados,
+        paginacao: {
+          total,
+          pagina,
+          limite,
+          totalPaginas,
+        },
+      };
     } catch (error) {
       console.error(":R - Erro ao buscar categorias.", error);
       throw error;
